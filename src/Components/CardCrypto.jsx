ebb2755a1a2e6773bdaffa526d99a02a78863305
¨Julian <juliangarciam13@hotmail.com>
2023-06-02 18:49:05 -0500

    The price format was modified and the containercards border was removed and the search for includes was modified to starswith

--- src/Components/CardCrypto.jsx
+++ src/Components/CardCrypto.jsx
@@ -27,10 +27,30 @@ const CardCrypto = () => {
     };
 
     const filteredCryptoData = cryptoData.filter((crypto) =>
-        crypto.name.toLowerCase().includes(searchValue) ||
-        crypto.symbol.toLowerCase().includes(searchValue)
+        crypto.name.toLowerCase().startsWith(searchValue) ||
+        crypto.symbol.toLowerCase().startsWith(searchValue)
     );
 
+    const formatNumber = (number) => {
+        return number.toLocaleString('es', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+            useGrouping: true,
+        }).replace(/\./g, ',');
+    };
+
+    const mapCrypto = () => {
+        return filteredCryptoData.map((crypto) => (
+            <div key={crypto.name} className='crypto'>
+                <img src={crypto.image} alt={crypto.name}></img>
+                <div className='nameSymbol'>
+                    <h4 style={{ fontSize: '12px' }}>{crypto.name}</h4>
+                    <p style={{ fontSize: '12px', color: '#898989' }}>{crypto.symbol}</p>
+                </div>
+                <p style={{ fontSize: '12px', fontWeight: 'bold', marginRight: '10px' }}>{formatNumber(crypto.current_price)}</p>
+            </div>
+        ));
+    };
 
     return (
         <div className="containerCards">
@@ -47,7 +67,7 @@ const CardCrypto = () => {
             <div className="containerPanel">
                 <div className="containerTitle">
                     <p style={{ fontSize: '130%' }}>Control panel</p>
-                    <input placeholder="         Enter your search request..." value={searchValue} onChange={handleSearchChange} />
+                    <input placeholder="Enter your search request..." value={searchValue} onChange={handleSearchChange} />
                     <div className='titleCrypto'>
                         <h1 style={{ fontSize: '80%' }}>B2B DISTRIBUTION</h1>
                         <p style={{ fontSize: '70%' }}>Sales Deals</p>
@@ -57,16 +77,7 @@ const CardCrypto = () => {
                     <hr className="horizontalLine" />
                 </div>
                 <div className="containerCryptos">
-                    {filteredCryptoData.map((crypto) => (
-                        <div key={crypto.name} className='crypto'>
-                            <img src={crypto.image} alt={crypto.name}></img>
-                            <div className='nameSymbol'>
-                                <h4 style={{ fontSize: '12px' }}>{crypto.name}</h4>
-                                <p style={{ fontSize: '12px', color: '#898989' }}>{crypto.symbol}</p>
-                            </div>
-                            <p style={{ fontSize: '12px', fontWeight: 'bold', marginRight: '10px' }}>${crypto.current_price}</p>
-                        </div>
-                    ))}
+                    {mapCrypto()}
                 </div>
             </div>
         </div>
