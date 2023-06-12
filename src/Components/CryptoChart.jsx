49c9440fa269ba358808ecba7b3cc117f30f5e50
¨Julian <juliangarciam13@hotmail.com>
2023-06-12 14:53:51 -0500

    The positioning of the name and price of the selected cryptocurrency has been improved, a loading status has been added to the graph and an alert has been added to the api requests to warn when the maximum data request is reached.

--- src/Components/CryptoChart.jsx
+++ src/Components/CryptoChart.jsx
@@ -1,50 +1,57 @@
 import Chart from "chart.js/auto";
+import '../Styles/CardCrypto.css';
 import React, { useState, useEffect } from "react";
 
+
 const CryptoChart = ({ selectedCrypto }) => {
-    const [chartInstance, setChartInstance] = useState(null);
+    const [chartInstance, setChartInstance] = useState();
+    const [loading, setLoading] = useState(true);
+
+    const fetchCryptoData = async () => {
+        try {
+            if (selectedCrypto) {
+                const response = await fetch(
+                    `https://api.coingecko.com/api/v3/coins/${selectedCrypto.id}/market_chart?vs_currency=usd&days=30`
+                );
+                const data = await response.json();
+
+                const dates = [];
+                const prices = [];
+
+                const startDate = new Date(data.prices[0][0]);
+                for (let i = 0; i < 30; i++) {
+                    const date = new Date(startDate);
+                    date.setDate(startDate.getDate() + i);
+                    dates.push(date.toLocaleDateString());
+                    prices.push(data.prices[i][1]);
+                }
+
+                const newChartData = {
+                    labels: dates,
+                    datasets: [
+                        {
+                            label: "Price(USD)",
+                            data: prices,
+                            backgroundColor: "rgba(192, 192, 192, 0.8)",
+                            borderWidth: 2,
+                            hoverBackgroundColor: "#C1EE0A",
+                        },
+                    ],
+                };
+
+                createChart(newChartData);
+                setLoading(false);
+            }
+        } catch (error) {
+            console.error(error);
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
-        const fetchCryptoData = async () => {
-            try {
-                if (selectedCrypto) {
-                    const response = await fetch(
-                        `https://api.coingecko.com/api/v3/coins/${selectedCrypto.id}/market_chart?vs_currency=usd&days=30`
-                    );
-                    const data = await response.json();
-
-                    const dates = [];
-                    const prices = [];
-
-                    const startDate = new Date(data.prices[0][0]);
-                    for (let i = 0; i < 30; i++) {
-                        const date = new Date(startDate);
-                        date.setDate(startDate.getDate() + i);
-                        dates.push(date.toLocaleDateString());
-                        prices.push(data.prices[i][1]);
-                    }
-
-                    const newChartData = {
-                        labels: dates,
-                        datasets: [
-                            {
-                                label: "Price(USD)",
-                                data: prices,
-                                backgroundColor: "rgba(192, 192, 192, 0.8)",
-                                borderWidth: 2,
-                                hoverBackgroundColor: "#C1EE0A",
-                            },
-                        ],
-                    };
-
-                    createChart(newChartData);
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
+        setLoading(true);
         fetchCryptoData();
+        setLoading(false);
     }, [selectedCrypto]);
 
     const createChart = (data) => {
@@ -88,8 +95,12 @@ const CryptoChart = ({ selectedCrypto }) => {
         setChartInstance(newChartInstance);
     };
 
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
     return (
-        <div style={{ width: "100%", height: "400px" }}>
+        <div style={{ width: "95%", height: "400px", marginLeft: '25px' }}>
             <canvas id="crypto-chart"></canvas>
         </div>
     );
